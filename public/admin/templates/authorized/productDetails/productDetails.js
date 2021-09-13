db117968ddd046bf491501eba2b4e6100c9fc492
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2021-09-13 17:14:27 +0600

    added product details view

--- public/admin/templates/authorized/productDetails/productDetails.js
+++ public/admin/templates/authorized/productDetails/productDetails.js
@@ -1,4 +1,4 @@
-app.controller('productDetails', function ($scope, $http, $routeParams) {
+app.controller('productDetails', function ($scope, $http, $routeParams, $location) {
     $scope.httpReq = async (url, body) => {
         $scope.isLoading = 1
         var req = {
@@ -18,8 +18,101 @@ app.controller('productDetails', function ($scope, $http, $routeParams) {
     }
     $scope.currentProductId = 0
 
-    $scope.initProductDetails = () => {
+    $scope.initProductDetails = async () => {
         $scope.currentProductId = $routeParams.productId
+        await $scope.getBasicInfo()
+        await $scope.getOwnerInfo()
         $('#productDetailsModal').modal('show')
+    }
+    $('#productDetailsModal').on('hidden.bs.modal', function () {
+        $scope.$apply(() => {
+            $location.path('/')
+        })
+    })
+    $scope.currentProduct = {
+        basicInfo: {},
+        customerList: {},
+        ownerInfo: {}
+    }
+    $scope.getCustomerList = () => { }
+    $scope.getOwnerInfo = async () => {
+        var ownerInfo = await $scope.httpReq(biden, {
+            query: `query{
+                        User(id:"${$scope.currentProduct.basicInfo.owner}"){
+                            id
+                            phoneNumber
+                            email
+                            address
+                            imageURL
+                            fullName
+                            notificationId
+                            createdOn
+                        }
+                    }`
+        })
+
+        $scope.$apply(function () {
+            $scope.currentProduct.ownerInfo = ownerInfo.User
+        })
+    }
+    $scope.getBasicInfo = async () => {
+        var basicInfo = await $scope.httpReq(biden, {
+            query: `query{
+                GetProductById(id:"${$scope.currentProductId}"){
+                  image1
+                  image2
+                  image3
+                  image4
+                  category
+                  postedOn
+                  askedPrice
+                  owner
+                  details
+                  customerCount
+                  postedFrom
+                }
+            }`
+        })
+        if (basicInfo == null) {
+            $scope.$apply(function () {
+                $location.path('/')
+            })
+        }
+        $scope.$apply(function () {
+            $scope.currentProduct.basicInfo = basicInfo.GetProductById
+            console.log($scope.currentProduct.basicInfo);
+        })
+
+    }
+    $scope.parseTime = (x) => {
+        var time = new Date(x * 1)
+        var res = time.getHours() + ":" + time.getMinutes() + " " + time.getDate() + '/' + time.getMonth() + '/' + time.getFullYear()
+        return res
+    }
+    $scope.viewUser = (x) => {
+        $location.path('/showUser/' + x)
+    }
+    $scope.canShowCustomers = 0
+    $scope.viewCustomers = async () => {
+        var Offerers = await $scope.httpReq(biden, {
+            query: `query{
+                GetProductById(id:"${$scope.currentProductId}"){
+                  Offerers{
+                    customerId
+                    offeredPrice
+                    whereToReceive
+                    time
+                    Buyer{
+                      fullName
+                      imageURL
+                    }
+                  }
+                }
+              }`
+        })
+        $scope.$apply(function () {
+            $scope.currentProduct.customerList = Offerers.GetProductById.Offerers
+            $scope.canShowCustomers = 1
+        })
     }
 })
